87a184417cff6c877b3de69a143f5f9c35dc22da
Russell Haering <russellhaering@gmail.com>
2010-10-10 14:40:14 -0700

    Tabs -> spaces in prog1.c

--- prog1.c
+++ prog1.c
@@ -27,7 +27,7 @@ void display() {
   long offset;
   hsv[1] = hsv[2] = 1.0;
 
-	glClear(GL_COLOR_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT);
 
   for (i = 0; i < set->y_dim; i++) {
     for (j = 0; j < set->x_dim; j++) {
@@ -52,18 +52,18 @@ void display() {
         glVertex2f(x + grid_width, y);
         glVertex2f(x + grid_width, y + grid_height);
         glVertex2f(x, y + grid_height);
-	    glEnd();
+      glEnd();
     }
   }
-	glFlush();
+  glFlush();
 }
 
 void initGL() {
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(0, WIDTH, HEIGHT, 0);
-	glMatrixMode(GL_MODELVIEW);
+  glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(0, WIDTH, HEIGHT, 0);
+  glMatrixMode(GL_MODELVIEW);
 }
 
 // the HSV color model will be as follows
@@ -75,43 +75,43 @@ void initGL() {
 // rgb is returned in 0-1 scale (ready for color3f)
 void HSVtoRGB(float hsv[3], float rgb[3]) {
   float f = (hsv[0] / 60.0f - (int) (hsv[0] / 60.0f));
-	float tmp1 = hsv[2] * (1 - hsv[1]);
-	float tmp2 = hsv[2] * (1 - hsv[1] * f);
-	float tmp3 = hsv[2] * (1 - hsv[1] * (1 - f));
-	switch((int)(hsv[0] / 60)) {
-		case 0:
-			rgb[0] = hsv[2];
-			rgb[1] = tmp3;
-			rgb[2] = tmp1;
-			break;
-		case 1:
-			rgb[0] = tmp2;
-			rgb[1] = hsv[2];
-			rgb[2] = tmp1;
-			break;
-		case 2:
-			rgb[0] = tmp1;
-			rgb[1] = hsv[2];
-			rgb[2] = tmp3;
-			break;
-		case 3:
-			rgb[0] = tmp1;
-			rgb[1] = tmp2;
-			rgb[2] = hsv[2];
-			break;
-		case 4:
-			rgb[0] = tmp3;
-			rgb[1] = tmp1;
-			rgb[2] = hsv[2];
-			break;
-    case 5:
-			rgb[0] = hsv[2];
-			rgb[1] = tmp1;
-			rgb[2] = tmp2;
+  float tmp1 = hsv[2] * (1 - hsv[1]);
+  float tmp2 = hsv[2] * (1 - hsv[1] * f);
+  float tmp3 = hsv[2] * (1 - hsv[1] * (1 - f));
+  switch((int)(hsv[0] / 60)) {
+    case 0:
+      rgb[0] = hsv[2];
+      rgb[1] = tmp3;
+      rgb[2] = tmp1;
       break;
-		default:
-			printf("What!? Inconceivable! (%d)\n", (int)(hsv[0] / 60));
-	}
+    case 1:
+      rgb[0] = tmp2;
+      rgb[1] = hsv[2];
+      rgb[2] = tmp1;
+      break;
+    case 2:
+      rgb[0] = tmp1;
+      rgb[1] = hsv[2];
+      rgb[2] = tmp3;
+      break;
+    case 3:
+      rgb[0] = tmp1;
+      rgb[1] = tmp2;
+      rgb[2] = hsv[2];
+      break;
+    case 4:
+      rgb[0] = tmp3;
+      rgb[1] = tmp1;
+      rgb[2] = hsv[2];
+      break;
+    case 5:
+      rgb[0] = hsv[2];
+      rgb[1] = tmp1;
+      rgb[2] = tmp2;
+      break;
+    default:
+      printf("What!? Inconceivable! (%d)\n", (int)(hsv[0] / 60));
+  }
 
 }
 
@@ -129,14 +129,14 @@ int main(int argc, char ** argv) {
   grid_width = ((WIDTH - 2) / set->x_dim);
   grid_height = ((HEIGHT - 2) / set->y_dim);
 
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(500, 500);
-	glutInitWindowPosition(0, 0);
-	glutCreateWindow("CS 450/550 Project 1");
-	glutDisplayFunc(display);
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutInitWindowSize(500, 500);
+  glutInitWindowPosition(0, 0);
+  glutCreateWindow("CS 450/550 Project 1");
+  glutDisplayFunc(display);
 
-	initGL();
-	glutMainLoop();
+  initGL();
+  glutMainLoop();
   return 0;
 }
